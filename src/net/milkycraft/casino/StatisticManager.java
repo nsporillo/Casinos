d90c91d6e3be7848ef3a1b0f883a6b546e74347c
Nick Porillo <milkywayz@mail.com>
2014-05-05 19:18:34 -0400

    Conventions

--- src/net/milkycraft/casino/StatisticManager.java
+++ src/net/milkycraft/casino/StatisticManager.java
@@ -161,6 +161,7 @@ public class StatisticManager {
 		LinkedList<Map.Entry<String, Integer>> list = new LinkedList<Map.Entry<String, Integer>>(
 				base.entrySet());
 		Collections.sort(list, new Comparator<Map.Entry<String, Integer>>() {
+			@Override
 			public int compare(Map.Entry<String, Integer> o1,
 					Map.Entry<String, Integer> o2) {
 				return o2.getValue().compareTo(o1.getValue());
