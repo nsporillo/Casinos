a867f5be413fe64c754606e29cd09b958600227c
Nick Porillo <milkywayz@mail.com>
2014-05-10 16:20:08 -0400

    Static import count

--- src/net/milkycraft/casino/Prize.java
+++ src/net/milkycraft/casino/Prize.java
@@ -2,6 +2,7 @@ package net.milkycraft.casino;
 
 import org.bukkit.inventory.ItemStack;
 import static org.bukkit.Material.AIR;
+import static net.milkycraft.casino.Count.*;
 
 public class Prize {
 
@@ -19,16 +20,16 @@ public class Prize {
 	public GameResult getResult() {
 		if (g(0).equals(g(1)) || g(1).equals(g(2)) || g(0).equals(g(2))) {
 			if (g(0).equals(g(1)) && g(1).equals(g(2))) {
-				return new GameResult(Count.THREE, g(0), out);
+				return new GameResult(THREE, g(0), out);
 			}
 			if (g(0).equals(g(1))) {
-				return new GameResult(Count.TWO, g(0), out);
+				return new GameResult(TWO, g(0), out);
 			} else if (g(1).equals(g(2))) {
-				return new GameResult(Count.TWO, g(1), out);
+				return new GameResult(TWO, g(1), out);
 			} else if (g(0).equals(g(2))) {
-				return new GameResult(Count.TWO, g(0), out);
+				return new GameResult(TWO, g(0), out);
 			}
 		}
-		return new GameResult(Count.LOST, new ItemStack(AIR), out);
+		return new GameResult(LOST, new ItemStack(AIR), out);
 	}
 }
